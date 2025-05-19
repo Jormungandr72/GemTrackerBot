e7ee5d71bea8b6fc1ba74bcd7a13b164c7ea0f00
Jormungandr72 <patrickjmcgranahan@gmail.com>
2025-05-19 15:30:44 -0500

    Ready to start testing sql script

--- src/Testing.py
+++ src/Testing.py
@@ -11,11 +11,14 @@ class Testing:
     -------------------------------------------------------------------------------
     Change Log:
     Who  When           What
+    PJM  05.19.2025     Put some sql code in to test with.
     PJM  05.19.2025     Added blueprint for a main program.
     PJM  05.19.2025     Created class Testing, created comment block.
     -------------------------------------------------------------------------------
     """
     def main():
+        DBManager.open_all()
+
         DBManager.cur.execute("""
         select ROWID, main_monster
         from quests
@@ -23,6 +26,8 @@ class Testing:
         """, (663063684620353586))
 
         print(DBManager.cur.fetchall())
+
+        DBManager.close_all()
 
     if __name__ == "__main__":
         main()
