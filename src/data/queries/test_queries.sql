3c82b7524a17b829a7f89eeca6f9dd4445841109
Jormungandr72 <patrickjmcgranahan@gmail.com>
2025-05-19 15:03:57 -0500

    Started testing queries for the quests command

--- src/data/queries/test_queries.sql
+++ src/data/queries/test_queries.sql
@@ -1,3 +1,7 @@
 -- SQLite, test misc sql statements here
-select ROWID from quests
-where ROWID = 1;
+insert into quests (main_monster, user_id) values ('Arkveld', 663063684620353586);
+insert into quests (main_monster, user_id) values ('Rathalos', 663063684620353586);
+insert into quests (main_monster, user_id) values ('Zoh Shia', 663063684620353586);
+
+select (ROWID, main_monster, user_id) from quests
+where user_id = '663063684620353586';
